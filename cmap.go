ea5791cf69afdf39808b87ecabe2c80a7226271a
fraczles <afracza1@binghamton.edu>
2015-03-26 09:45:36 -0400

    changed Cmap to cmap

--- cmap.go
+++ cmap.go
@@ -1,9 +1,9 @@
 package main
 
-type Cmap struct{
+type cmap struct{
 //Create structure here
 }
 
-func NewChannelMap() * Cmap {
+func NewChannelMap() * cmap {
     //TODO: Populate and return new map
 }
